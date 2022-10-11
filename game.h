fe05b5458c470e5a151e6f4cffb3271cc535f0fc
Gachiyabakune <fko2247097@stu.o-hara.ac.jp>
2022-10-11 10:26:27 +0900

    画面サイズ変更

--- game.h
+++ game.h
@@ -6,8 +6,8 @@ namespace Game
 	constexpr bool kWindowmode = true;
 	const char* const kTitleText = "ＧＡＭＥ";
 	//ウィンドウサイズ
-	constexpr int kScreenWidth = 1280;
-	constexpr int kScreenHight = 720;
+	constexpr int kScreenWidth = 460;
+	constexpr int kScreenHight = 820;
 	//カラーモード
 	constexpr int kColorDepth = 32;   //32 or 16
 
